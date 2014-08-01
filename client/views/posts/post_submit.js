8f47b844c88cdd070e5eefe63bf89a83034a63d5
cuongcua90 <cuongcua90@hotmail.com>
2014-08-01 17:23:52 +0700

    youtube link submit

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,10 +1,14 @@
+var youtubeUrlPrefix = function(id){
+	// https://gdata.youtube.com/feeds/api/videos/psx-eHmqBvE?v=2&alt=json
+	return 'https://gdata.youtube.com/feeds/api/videos/'+id+'?v=2&alt=json';
+};
 Template.postSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
 		var post = {
 			url: $(e.target).find('[name=url]').val(),
-			title: $(e.target).find('[name=title]').val(),
-			message: $(e.target).find('[name=message]').val()
+			// title: $(e.target).find('[name=title]').val(),
+			// message: $(e.target).find('[name=message]').val()
 		}
 		Meteor.call('post',post, function(error,id){
 			if (error) {
@@ -13,6 +17,30 @@ Template.postSubmit.events({
 				Router.go('postPage', {_id: id});
 			}
 		})
+	},
+	'click #youtubeinfo':function(e) {
+		// alert('hehe');
+		var url = document.getElementById('youtubeurl').value;
+		// alert(url);
+		var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+		var youtubeUrlApi = youtubeUrlPrefix(videoid[1]);
+		var jqxhr = $.get(youtubeUrlApi, function(responseTxt, statusTxt, xhr) {
+            if (responseTxt){
+            	var responseMedia = responseTxt['entry']['media$group'];
+            	// console.log(responseTxt['entry']['media$group']);
+            	youtubeInfo = {
+            		thumbnail: responseMedia['media$thumbnail'][3]['url'],
+            		title: responseMedia['media$title']['$t']
+            	};
+            	Posts.insert({
+            		videoId: videoid[1],
+            		title: youtubeInfo.title,
+            		videoThumbnail: youtubeInfo.thumbnail
+            	});
+            	console.log(youtubeInfo);
+            }
+        });
 	}
+
 });
 
--- collections/posts.js
+++ collections/posts.js
@@ -1,21 +1,39 @@
 Posts = new Meteor.Collection("posts");
-
+console.log('helo');
 Meteor.methods({
 	post: function(postAttributes) {
+		console.log(postAttributes);
 		var user = Meteor.user(),
 		postWithSameLink = Posts.findOne({url: postAttributes.url});
 		// ensure the user is logged in
-		if (!user)
-			throw new Meteor.Error(401, "You need to login to post new stories");
-		// ensure the post has a title
-		if (!postAttributes.title)
-			throw new Meteor.Error(422, 'Please fill in a headline');
-		// check that there are no previous posts with the same link
-		if (postAttributes.url && postWithSameLink) {
-			throw new Meteor.Error(302,
-				'This link has already been posted',
-				postWithSameLink._id);
-		}
+		// if (!user)
+		// 	throw new Meteor.Error(401, "You need to login to post new stories");
+		// // ensure the post has a title
+		// if (!postAttributes.title)
+		// 	throw new Meteor.Error(422, 'Please fill in a headline');
+		// // check that there are no previous posts with the same link
+		// if (postAttributes.url && postWithSameLink) {
+		// 	throw new Meteor.Error(302,
+		// 		'This link has already been posted',
+		// 		postWithSameLink._id);
+		// }
+
+		var videoid = postAtributes.url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+		var youtubeUrlApi = youtubeUrlPrefix(videoid[1]);
+		console.log(videoid[1]);
+		var jqxhr = $.get(youtubeUrlApi, function(responseTxt, statusTxt, xhr) {
+            if (responseTxt){
+            	var responseMedia = responseTxt['entry']['media$group'];
+            	// console.log(responseTxt['entry']['media$group']);
+            	youtubeInfo = {
+            		thumbnail: responseMedia['media$thumbnail'][3]['url'],
+            		title: responseMedia['media$title']['$t']
+            	}
+            	console.log(youtubeInfo);
+            }
+        });
+
+
 		// pick out the whitelisted keys
 		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
 			userId: user._id,
