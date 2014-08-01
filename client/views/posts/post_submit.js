671ff65fa68fa0b1ef06dbc23dcfcff551d7a138
cuongcua90 <cuongcua90@hotmail.com>
2014-08-01 18:09:01 +0700

    submit and list post

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -2,21 +2,36 @@ var youtubeUrlPrefix = function(id){
 	// https://gdata.youtube.com/feeds/api/videos/psx-eHmqBvE?v=2&alt=json
 	return 'https://gdata.youtube.com/feeds/api/videos/'+id+'?v=2&alt=json';
 };
+var thumbnailLink = function(id){
+	return 'https://i.ytimg.com/vi/'+id+'/mqdefault.jpg'
+}
 Template.postSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
-		var post = {
-			url: $(e.target).find('[name=url]').val(),
-			// title: $(e.target).find('[name=title]').val(),
-			// message: $(e.target).find('[name=message]').val()
-		}
-		Meteor.call('post',post, function(error,id){
-			if (error) {
-				throwError(error.reason);
-			} else {
-				Router.go('postPage', {_id: id});
-			}
-		})
+		// alert('hehe');
+		var url = document.getElementById('youtubeurl').value;
+		// alert(url);
+		var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+		var youtubeUrlApi = youtubeUrlPrefix(videoid[1]);
+		var jqxhr = $.get(youtubeUrlApi, function(responseTxt, statusTxt, xhr) {
+            if (responseTxt){
+            	var responseMedia = responseTxt['entry']['media$group'];
+            	// console.log(responseTxt['entry']['media$group']);
+            	youtubeInfo = {
+            		thumbnail: thumbnailLink(videoid[1]),
+            		title: responseMedia['media$title']['$t']
+            	};
+            	Posts.insert({
+            		videoId: videoid[1],
+            		title: youtubeInfo.title,
+            		videoThumbnail: youtubeInfo.thumbnail
+            	},function(err,id){
+            		Router.go('postPage', {_id: id});
+            	});
+            	
+            	// console.log(youtubeInfo);
+            }
+        });
 	},
 	'click #youtubeinfo':function(e) {
 		// alert('hehe');
@@ -29,7 +44,7 @@ Template.postSubmit.events({
             	var responseMedia = responseTxt['entry']['media$group'];
             	// console.log(responseTxt['entry']['media$group']);
             	youtubeInfo = {
-            		thumbnail: responseMedia['media$thumbnail'][3]['url'],
+            		thumbnail: thumbnailLink(videoid[1]),
             		title: responseMedia['media$title']['$t']
             	};
             	Posts.insert({
