683371d92cce6edb2f869ecde68dc20d40d46f29
phienkaka <phiencoltech@gmail.com>
2014-08-12 20:12:17 +0000

    debug

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -2,13 +2,9 @@
 Template.postItem.helpers({
 	ownPost: function(){
 		return this.userId === Meteor.userId();
-	},
+	}
 	//todos: progress
 });
 
 Template.postItem.rendered = function(){
-    $('.editable').editable({
-		  success: function(response, newValue) {
-		  	alert(this);
-		}});
-  }
+}
