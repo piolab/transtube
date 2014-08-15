c7f85887917a0e0b1e0116840f024019a64fd9d9
cuongcua90 <cuongcua90@hotmail.com>
2014-08-15 15:39:30 +0700

    login by google

--- collections/posts.js
+++ collections/posts.js
@@ -3,19 +3,20 @@
  Meteor.methods({
  	post: function(post){
  		var user = Meteor.user();
+ 		console.log(user);
  		var duplicatePost = Posts.findOne({videoId: post.videoId});
 
  		if (!user) {
  			throw new Meteor.Error(401, "You need to login");
  		}
- 		else if (duplicatePost){
- 			throw new Meteor.Error(302, "This link has already been posted",duplicatePost._id);
- 		}
+ 		// else if (duplicatePost){
+ 		// 	throw new Meteor.Error(302, "This link has already been posted",duplicatePost._id);
+ 		// }
  		else{
  			// Initlize post
  			post.submited = new Date().getTime();
  			post.userId = user._id;
- 			post.author = user.email;
+ 			post.author = user["emails"][0]["address"];
 
  			var postId = Posts.insert(post);
  			return postId;
